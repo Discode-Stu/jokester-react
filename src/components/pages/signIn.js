f308c61e7a4eb71ace0f89719bf20292a0b8b6d4
Discode Stu <dstuart85@yahoo.com>
2020-09-05 18:15:03 -0400

    created and styled pageTitle component

--- src/components/pages/signIn.js
+++ src/components/pages/signIn.js
@@ -1,8 +1,7 @@
 import React, { Component } from 'react';
 
 import { SignInForm } from './signInForm';
-// import SimpleForm from './signInForm';
-
+import PageTitle from '../pageTitle/pageTitle';
 export default class SignIn extends Component {
 
     onSubmit = (fields) => {
@@ -12,8 +11,8 @@ export default class SignIn extends Component {
     render() {
         return (
             <div className='sign-in'>
+                <PageTitle className='sign-in__page-title' title='Login' />
                 <SignInForm onSubmit={this.onSubmit} className='sign-in__form' />
-                {/* <SimpleForm onSubmit={this.onSubmit} className='sign-in__form' /> */}
             </div>
         );
     }
